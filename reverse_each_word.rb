ca9c16108bd50e17d4f8de6f3a3d9bc63f6d2cdd
Dominique Skaf <doskaf@yahoo.com>
2019-08-24 21:43:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,10 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  original_array = 
-end
+  array = string.split(" ")
+  return_array = []
+  array.collect do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+  end
